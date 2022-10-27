e736b7c26449404aa7b554406da1a8edb8bbba2e
Son <70love07@naver.com>
2022-10-28 07:40:07 +0900

    회원 인증을 위한 기능이 모두 구현. 회원 인증 기능을 구현하는 과정에서, 우리는 UI를 구성하고 애플리케이션에 필요한 상태 관리추가, API를 연동하는 과정에서 리덕스와 redux-saga를 사용

---
+++ src/components/base/Header.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+/**
+ * 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트입니다.
+ */
+
+const Headerblock = styled.div``;
+
+const Header= () => {
+    return (
+    <Headerblock>
+    
+    </Headerblock>
+  );
+};
+
+export default Header;
